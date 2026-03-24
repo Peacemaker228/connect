1bcc35a452a51b025de7bbf4b40fe257da06203e
alan.batchaev <alanchik99@gmail.com>
2026-03-24 13:32:35 +0300

    core: fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin()
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
