1d874c334fd4f7492883b190f839334b5465bd55
alan.batchaev <alanchik99@gmail.com>
2026-03-24 21:49:57 +0300

    core: Update Clerk proxy setup 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/uploadthing(.*)'])
 
-const useFrontendApiProxy = !!process.env.NEXT_PUBLIC_CLERK_PROXY_URL
-
-export default clerkMiddleware(
-  async (auth, request) => {
-    if (!isPublicRoute(request)) {
-      await auth.protect()
-    }
-  },
-  useFrontendApiProxy
-    ? {
-        frontendApiProxy: {
-          enabled: true,
-        },
-      }
-    : {},
-)
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc|__clerk)(.*)',
+    '/(api|trpc)(.*)',
   ],
 }
