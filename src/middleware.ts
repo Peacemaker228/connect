8ab96ed60275636e4c9120e772e2b892c1085d39
Alan <alanchik99@gmail.com>
2026-06-09 11:09:16 +0300

    core/middleware: enable frontendApiProxy for clerk and update route matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,24 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/', '/invite(.*)', '/sign-in(.*)', '/sign-up(.*)', '/api/uploadthing(.*)'])
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
-  }
-})
+export default clerkMiddleware(
+  async (auth, request) => {
+    if (!isPublicRoute(request)) {
+      await auth.protect()
+    }
+  },
+  {
+    frontendApiProxy: {
+      enabled: true,
+      path: '/__clerk',
+    },
+  },
+)
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
+    '/__clerk/(.*)',
   ],
 }
